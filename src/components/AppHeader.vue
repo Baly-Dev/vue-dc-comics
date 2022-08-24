f0d0a51cb66d52f5e2e6cd17a2004fa9e0baf34c
baly <kevin@baldev.it>
2022-08-24 22:43:08 +0200

    Commit from terminal

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="d-flex container">
+        <div class="first">
+            <img src="../assets/dc-logo.png" alt="DC Logo">
+        </div>
+        <nav class="second nav-bar">
+            <ul class="menu">
+                <li v-for="menuItem in menuItems" :key="menuItem" class="menu-item">
+                    <a href="">{{ menuItem }}</a>
+                </li>
+            </ul>
+        </nav>      
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    data(){
+        return{
+            menuItems: [
+            'Characters',
+            'Comics',
+            'Movies',
+            'Tv',
+            'Games',
+            'Collectibles',
+            'Videos',
+            'Fans',
+            'News',
+            'Shop'
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+    @import './src/styles/header.scss';
+
+</style>
+
